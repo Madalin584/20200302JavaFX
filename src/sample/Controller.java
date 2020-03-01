856c611869acce6c8fe1809b60fe0f10627ad634
Madalin584 <madalinmadalin223@gmail.com>
2020-03-01 23:33:35 +0200

    login ok

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,50 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.Objects;
+
 public class Controller {
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+
+    public void login() {
+        System.out.println("test");
+        System.out.println(username.getText() + " " + password.getText());
+        if (validate()) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setContentText("You have logged in succesfully");
+            alert.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("You have failed!!!!");
+            alert.show();
+        }
+    }
+
+    public boolean validate() {
+        if (username.getText().equals("gigi") && password.getText().equals("1234")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Controller)) return false;
+        Controller that = (Controller) o;
+        return username.equals(that.username) &&
+                password.equals(that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
 }
